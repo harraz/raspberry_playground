2301a7de4a3c4184d0b03b8e2826db8bc1248d71
mohamed f. harraz <harraz@gmail.com>
2024-11-20 20:02:01 -0500

    added listen moduile

---
+++ esp8266/listen_to_ard.py
@@ -0,0 +1,12 @@
+import socket
+
+local_ip = "192.168.1.206"  # Raspberry Pi IP
+local_port = 8080           # Port to listen on
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind((local_ip, local_port))
+
+print("Listening for responses...")
+while True:
+    data, addr = sock.recvfrom(1024)  # Buffer size 1024 bytes
+    print(f"Received response: {data.decode()} from {addr}")
